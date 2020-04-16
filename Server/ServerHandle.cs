e52ed3b874fd1d59b3d0d6944440c3581e3a283b
lancer <lancelot.krumpholz@outlook.com>
2020-04-16 16:45:07 +1000

    Server Added

---
+++ Server/ServerHandle.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace GameServer
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient} called {_username}");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SendIntoGame(_username);
+        }
+
+        public static void PlayerMovement(int _fromClient, Packet _packet)
+        {
+            bool[] _inputs = new bool[_packet.ReadInt()];
+            for (int i = 0; i < _inputs.Length; i++)
+            {
+                _inputs[i] = _packet.ReadBool();
+            }
+            Quaternion _rotation = _packet.ReadQuaternion();
+            if (Server.clients[_fromClient].player !=null)
+            {
+                Server.clients[_fromClient].player.SetInput(_inputs, _rotation);
+            }
+        }
+
+        public static void PlayerAttack(int _fromClient, Packet _packet)
+        {
+            int _id = _packet.ReadInt();
+            if (Server.clients[_fromClient].player != null)
+            {
+                ServerSend.PlayerAttacked(Server.clients[_fromClient].player, Server.clients[_id].player);
+
+                Server.clients[_id].player.hp -= 1;
+
+                if(Server.clients[_id].player.hp <= 0)
+                {
+                    ServerSend.PlayerDown(Server.clients[_id].player);
+
+                    int i = 0;
+                    Player winner = null;
+                    foreach (Client _client in Server.clients.Values)
+                    {
+                        if (_client.player != null && _client.player.hp > 0)
+                        {
+                            i++;
+                            winner = _client.player;
+                        }
+                    }
+                    if (i == 1) {
+                        ServerSend.EndGame(winner);
+                        // ELIMINATE ALL PLAYER DATA, RESET GAME, fix camera on victory.
+                        // lesch olle jusa!
+                        foreach (Client _client in Server.clients.Values)
+                        {
+                            _client.Disconnect();
+                                
+                        }
+                        Server.clients = new Dictionary<int, Client>();
+                        Server.InitializeServerData();
+
+                    }
+
+
+                }
+
+
+            }
+        }
+
+
+        public static void PlayerMiss(int _fromClient, Packet _packet)
+        {
+            if (Server.clients[_fromClient].player != null)
+            {
+                ServerSend.PlayerMissed(Server.clients[_fromClient].player);
+            }
+        }
+    }
+}
---
+++ Server/ServerSend.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameServer
+{
+    class ServerSend
+    {
+        private static void SendTCPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Server.clients[_toClient].tcp.SendData(_packet);
+        }
+
+        private static void SendUDPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Server.clients[_toClient].udp.SendData(_packet);
+        }
+
+        private static void SendTCPDataToAll(Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                Server.clients[i].tcp.SendData(_packet);
+            }
+        }
+        private static void SendTCPDataToAll(int _exceptClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                if (i != _exceptClient)
+                {
+                    Server.clients[i].tcp.SendData(_packet);
+                }
+            }
+        }
+
+        private static void SendUDPDataToAll(Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                Server.clients[i].udp.SendData(_packet);
+            }
+        }
+        private static void SendUDPDataToAll(int _exceptClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= Server.MaxPlayers; i++)
+            {
+                if (i != _exceptClient)
+                {
+                    Server.clients[i].udp.SendData(_packet);
+                }
+            }
+        }
+
+        #region Packets
+        public static void Welcome(int _toClient, string _msg)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.welcome))
+            {
+                _packet.Write(_msg);
+                _packet.Write(_toClient);
+
+                SendTCPData(_toClient, _packet);
+            }
+        }
+
+        public static void hp(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.hp))
+            {
+                _packet.Write(_player.id);
+                _packet.Write(_player.hp);
+
+                SendUDPDataToAll(_packet);
+            }
+        }
+
+
+
+        public static void PlayerAttacked(Player _attacker, Player _target)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerAttacked))
+            {
+                {
+                    _packet.Write(_attacker.id);
+                    _packet.Write(_target.id);
+
+                    SendTCPDataToAll(_packet);
+                }
+            }
+        }
+
+        public static void PlayerDown(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerDown))
+            {
+                {
+                    _packet.Write(_player.id);
+
+                    SendTCPDataToAll(_packet);
+                }
+            }
+        }
+
+        public static void EndGame(Player winner)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.endGame))
+            {
+                {
+                    _packet.Write(winner.id);
+                    SendTCPDataToAll(_packet);
+                }
+            }
+        }
+
+
+        public static void PlayerMissed(Player _attacker)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerMissed))
+            {
+                {
+                    _packet.Write(_attacker.id);
+
+                    SendTCPDataToAll(_packet);
+                }
+            }
+        }
+
+        public static void SpawnPlayer(int _toClient, Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.spawnPlayer))
+            {
+                _packet.Write(_player.id);
+                _packet.Write(_player.username);
+                _packet.Write(_player.position);
+                _packet.Write(_player.rotation);
+
+                SendTCPData(_toClient, _packet);
+            }
+        }
+
+        public static void PlayerPosition(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerPosition))
+            {
+                _packet.Write(_player.id);
+                _packet.Write(_player.position);
+
+                SendUDPDataToAll(_packet);
+            }
+        }
+
+        public static void RemovePlayer(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.removePlayer))
+            {
+                _packet.Write(_player.id);
+
+                SendTCPDataToAll(_packet);
+            }
+        }
+
+
+        public static void PlayerRotation(Player _player)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.playerRotation))
+            {
+                _packet.Write(_player.id);
+                _packet.Write(_player.rotation);
+
+                SendUDPDataToAll(_player.id, _packet);
+            }
+        }
+        #endregion
+    }
+}
